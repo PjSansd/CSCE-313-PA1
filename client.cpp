3ff55f914a4b61165a272a56a8d89ea01f40ebac
PjSansd <harryhaihuapop@gmail.com>
2025-09-26 22:38:52 +0000

    added 1000 readings

--- client.cpp
+++ client.cpp
@@ -5,7 +5,7 @@
     Texas A&M University
     Date: 2/8/20
 	
-	Please include your Name, UIN, and the date below
+
 	Name: Haihua Pei
 	UIN: 333005716
 	Date: 09/23/2025
@@ -13,15 +13,16 @@
 #include "common.h"
 #include "FIFORequestChannel.h"
 #include <sys/wait.h>
+#include <fstream>
 
 using namespace std;
 
 
 int main (int argc, char *argv[]) {
 	int opt;
-	int p = 1;
-	double t = 0.0;
-	int e = 1;
+	int p = -1;
+	double t = -1.0;
+	int e = -1;
 	
 	string filename = "";
 	while ((opt = getopt(argc, argv, "p:t:e:f:")) != -1) {
@@ -56,7 +57,9 @@ int main (int argc, char *argv[]) {
 		cout << "Fork Successful. \n";
 
 		// You can add "-m buffer_size" to increase buffer size
-		execl("server.cpp", "server", NULL);
+		execl("server", "server", NULL);
+
+		cout << "this shouldn't print.";
 	}
 
 
@@ -64,30 +67,142 @@ int main (int argc, char *argv[]) {
     FIFORequestChannel chan("control", FIFORequestChannel::CLIENT_SIDE);
 	
 	// example data point request
-    char buf[MAX_MESSAGE]; // 256
-    datamsg x(1, 0.0, 1);
+    // char buf[MAX_MESSAGE]; // 256
+    // datamsg x(1, 0.0, 1);
 	
-	memcpy(buf, &x, sizeof(datamsg));
-	chan.cwrite(buf, sizeof(datamsg)); // question
-	double reply;
-	chan.cread(&reply, sizeof(double)); //answer
-	cout << "For person " << p << ", at time " << t << ", the value of ecg " << e << " is " << reply << endl;
-	
-    // sending a non-sense message, you need to change this
-	filemsg fm(0, 0);
-	string fname = "teslkansdlkjflasjdf.dat";
-	
-	int len = sizeof(filemsg) + (fname.size() + 1);
-	char* buf2 = new char[len];
-	memcpy(buf2, &fm, sizeof(filemsg));
-	strcpy(buf2 + sizeof(filemsg), fname.c_str());
-	chan.cwrite(buf2, len);  // I want the file length;
+	// memcpy(buf, &x, sizeof(datamsg));
+	// chan.cwrite(buf, sizeof(datamsg)); // question
+	// double reply;
+	// chan.cread(&reply, sizeof(double)); //answer
+	// cout << "For person " << p << ", at time " << t << ", the value of ecg " << e << " is " << reply << endl;
 
-	delete[] buf2;
+	// Data Point Request
+	char dataBuffer[MAX_MESSAGE]; // 256
+
+	// If requesting filename: 
+	if (filename != "") {
+		cout << " Starting File Transfer. " << endl;
+
+		// Send initial request/handshake:
+
+		filemsg fm(0, 0);
+		
+		int len = sizeof(filemsg) + (filename.size() + 1);
+		char* buf2 = new char[len];
+		memcpy(buf2, &fm, sizeof(filemsg));
+		strcpy(buf2 + sizeof(filemsg), filename.c_str());
+		chan.cwrite(buf2, len);  // I want the file length;
+
+		delete[] buf2;
+
+		__int64_t response; 
+		chan.cread(&response, sizeof(__int64_t));
+
+		cout << "file length:" << response;
+
+		// Gotten file length
+
+
+
+		// Prepare file to write
+		fstream ofs;
+		ofs.open( "received/" + filename, ios::out | ios::trunc );
+
+		ofstream writeFile("received/" + filename);
+
+		if (!writeFile.is_open()) {
+			cout << "invalid file name." << endl;
+			return 1;
+		}
+
+
+
+		//start downloading the file through bits and bytes
+		for ( __int64_t offsetStart = 0; offsetStart < response; offsetStart += MAX_MESSAGE) {
+
+			filemsg fileRequestMsg(offsetStart, (int) min( (__int64_t) MAX_MESSAGE, response - offsetStart) );
+
+			// Create a message that sends the filename AFTER everything aswell
+			int len = sizeof(filemsg) + (filename.size() + 1);
+			char* buf2 = new char[len];
+			memcpy(buf2, &fileRequestMsg, sizeof(filemsg));
+			strcpy(buf2 + sizeof(filemsg), filename.c_str());
+			chan.cwrite(buf2, len);		
+			delete[] buf2;
+			// This should work now
+
+			char* fileResponse[MAX_MESSAGE];
+			chan.cread(&fileResponse, sizeof(fileResponse));
+
+			// Start writing to file 
+
+			writeFile << fileResponse;
+			
+		}
+
+		writeFile.close();
+
+	} else { // If not requesting full file
+
+		
+		if (p < 1 || p > 15) {
+			cout << " P flag invalid or not entered." << endl;
+		} else if (t == -1 || e == -1) { 
+			cout << " t and or e flag is not entered. Sending entire p file. " << endl;
+
+			fstream ofs;
+			ofs.open( "received/x1.csv", ios::out | ios::trunc );
+
+			ofstream writeFile("received/x1.csv");
+
+			if (! writeFile.is_open()) {
+				cout << "unable to write to received." << endl;
+				return 1;
+			}
+
+			for ( double i = 0; i < 0.004 * 1000; i+= 0.004 ){ 
+				//ecg1: 
+				datamsg x1(p, i, 1);
+
+				memcpy(dataBuffer, &x1, sizeof(datamsg));
+				chan.cwrite(dataBuffer, sizeof(datamsg));
+
+				double dataReply1; 
+				chan.cread(&dataReply1, sizeof(double));
+
+				//ecg2: 
+				datamsg x2(p, i, 2);
+				memcpy(dataBuffer, &x2, sizeof(datamsg));
+				chan.cwrite(dataBuffer, sizeof(datamsg));
+
+				double dataReply2; 
+				chan.cread(&dataReply2, sizeof(double));
+
+				writeFile << i << "," << dataReply1 << "," << dataReply2 << "\n";
+
+			}
+
+			writeFile.close();
+			
+
+		} else { 
+
+			cout << "Proceeding as normal. " << endl;
+			datamsg y(p, t, e); // Person, Seconds, Eno
+
+			memcpy(dataBuffer, &y, sizeof(datamsg));
+			chan.cwrite(dataBuffer, sizeof(datamsg));
+
+			double dataReply; 
+			chan.cread(&dataReply, sizeof(double));
+			cout << "For person " << p << ", at time " << t << ", the value of ecg " << e << " is " << dataReply << endl;
+		}
+	}
 	
 	// closing the channel    
     MESSAGE_TYPE m = QUIT_MSG;
     chan.cwrite(&m, sizeof(MESSAGE_TYPE));
+	// cout << "cwritten" << endl;
 
-	wait(&status);
+	wait(&status); 
 }
